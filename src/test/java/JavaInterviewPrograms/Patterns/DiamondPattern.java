4cab946929da31052998389c4b858778c94ef21e
Gokulnath Sundaramoorthy <gokulnath.s24@gmail.com>
2025-11-06 12:30:41 -0600

    Additional Pattern Program
    Additional Pattern Program

--- src/test/java/JavaInterviewPrograms/Patterns/DiamondPattern.java
+++ src/test/java/JavaInterviewPrograms/Patterns/DiamondPattern.java
@@ -1,4 +1,30 @@
 package JavaInterviewPrograms.Patterns;
 
 public class DiamondPattern {
+
+    //Java program to print a Diamond pattern using *
+
+    public static void main(String[] args) {
+        int n = 5;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = i; j < n; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= (2 * i - 1); k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        for (int i = n-1; i >= 1; i--) {
+            for (int j = n; j > i; j--) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= (2 * i - 1); k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
 }
